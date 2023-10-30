de59bbdaf26726c8831d8a50056ec56fb4409547
ExthemWP <exthem.appstore@gmail.com>
2023-10-30 05:46:48 +0300

    authored

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAe0irog3ozvOeuImhiYHTbEQD1JLJEidg",
+    authDomain: "privacy-policy-generator-34c32.firebaseapp.com",
+      projectId: "privacy-policy-generator-34c32",
+        storageBucket: "privacy-policy-generator-34c32.appspot.com",
+          messagingSenderId: "1096187024012",
+            appId: "1:1096187024012:web:29ecc6b93b6dbdd7ae21a9"
+            };
+
+            // Initialize Firebase
+            const app = initializeApp(firebaseConfig);
